90fc519704c7a79ac6cb68e1e4556c3aca2c03bc
yunweiwo <195343918@qq.com>
2015-11-25 20:32:50 +0800

    Commit Test

--- Test/MyJava/src/IO/TestIO.java
+++ Test/MyJava/src/IO/TestIO.java
@@ -2,9 +2,9 @@ package IO;
 
 public class TestIO {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		System.out.println("test");
 	}
 
 }
